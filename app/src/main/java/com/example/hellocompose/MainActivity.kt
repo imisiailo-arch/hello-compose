3c8dfb8430f7494f04c4a2ca7ef680329eef7650
Imisi <imisiailo@gmail.com>
2026-05-27 18:10:35 +0300

    Update greeting text

--- app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             HelloComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Студент",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
